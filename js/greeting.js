6af0b686e63ccc2961b7c4745b4cbaf0cbc0c0b2
Ciro Mota <ciro.mota@gmail.com>
2023-08-06 14:49:44 -0300

    fix: Fixed ts(6385).

--- js/greeting.js
+++ js/greeting.js
@@ -2,18 +2,18 @@ var today = new Date();
 var Hr = today.getHours();
 
 // Here you can change the greetings and your name
-var name = 'Ciro, ' ;
+var myname = 'Ciro, ' ;
 var lateTxt = 'GO TO SLEEP!!!';
 var morningTxt = 'Good morning!';
 var afterTxt = 'Good afternoon!';
 var evenTxt = 'Good evening!';
 
 if (Hr >= 0 && Hr < 6) {
-    document.getElementById('greetings').innerText = name + lateTxt;
+    document.getElementBy('greetings').innerText = myname + lateTxt;
 } else if (Hr >= 6 && Hr < 12) {
-    document.getElementById('greetings').innerText = name + morningTxt;
+    document.getElementById('greetings').innerText = myname + morningTxt;
 } else if (Hr >= 12 && Hr < 18) {
-    document.getElementById('greetings').innerText = name + afterTxt;
+    document.getElementById('greetings').innerText = myname + afterTxt;
 } else {
-    document.getElementById('greetings').innerText = name + evenTxt;
+    document.getElementById('greetings').innerText = myname + evenTxt;
 }
