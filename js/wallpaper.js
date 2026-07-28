3287e1fee36c14e2766caf352951e8a5407293ad
Ciro Mota <ciro.mota@gmail.com>
2026-07-28 10:30:23 -0300

    feat(wallpaper): use Bing image API for background instead of Reddit

--- js/wallpaper.js
+++ js/wallpaper.js
@@ -1,6 +1,3 @@
-$.getJSON("https://www.reddit.com/r/EarthPorn/.json", function(data) { 
-    $.each(data.data.children, function(i,item){
-        const bgPath = item.data.url;
-        document.querySelector('html').style.backgroundImage = "url(" + bgPath + ")";
-    });
-});
+const period = "random"; // or "0" for Bing today image.
+
+document.querySelector('html').style.backgroundImage = "url(https://bing.biturl.top/?resolution=1920&format=image&index=" + period + "&mkt=en-US)";
